0552456408270f822fbc865cb2c130e304414fb4
Tom Richards <tom@tomrichards.net>
2015-06-08 16:22:52 -0400

    Organize code for easier date maintenance

--- assets/main.js
+++ assets/main.js
@@ -1,23 +1,22 @@
+var currentRiverbendStart = new Date('June 5, 2015 17:00:00');
+var currentRiverbendEnd = new Date('June 14, 2015 23:30:00');
+var nextRiverbendStart = new Date('June 5, 2016 17:00:00');
 var today = new Date();
-var startdate = new Date('June 5, 2015 17:00:00');
-var enddate =  new Date('June 14, 2015 23:30:00');
 
-if (today >= startdate & today <= enddate) {
+if (today >= currentRiverbendStart && today <= currentRiverbendEnd) {
     document.getElementById('response').innerHTML = 'Nope.';
     document.getElementById('text').innerHTML = "The riverfront is trampled & downtown is covered with trash. <br />It's almost done. Hang in there.";
-    CountDownTimer('06/14/2015 11:30 PM', 'countdown');
+    CountDownTimer(currentRiverbendEnd, 'countdown');
 }
 else {
     document.getElementById('response').innerHTML = 'Yep.';
     document.getElementById('text').innerHTML = "But don't get too excited. The whole thing starts again in:";
-    CountDownTimer('06/06/2016 5:30 PM', 'countdown');
+    CountDownTimer(nextRiverbendStart, 'countdown');
 }
 
 // Borrowed from http://stackoverflow.com/questions/9335140/how-to-countdown-to-a-date
-function CountDownTimer(dt, id)
+function CountDownTimer(end, id)
 {
-    var end = new Date(dt);
-
     var _second = 1000;
     var _minute = _second * 60;
     var _hour = _minute * 60;
