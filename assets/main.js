cc7475950009ba1b00b5b5b19da43b695f0d71b1
Tom Richards <tom@tomrichards.net>
2015-06-09 12:50:44 -0400

    Replace borrowed code with clean implementation

--- assets/main.js
+++ assets/main.js
@@ -1,49 +1,28 @@
-var currentRiverbendStart = new Date('June 5, 2015 17:00:00');
-var currentRiverbendEnd = new Date('June 13, 2015 23:30:00');
-var nextRiverbendStart = new Date('June 5, 2016 17:00:00');
-var today = new Date();
+$(function() {
+    var riverbendStart = moment('2015-06-05T17:00:00-04:00');
+    var riverbendEnd = moment('2015-06-13T23:30:00-04:00');
+    var futureEventStart = moment('2016-06-05T17:00:00-04:00');
 
-if (today >= currentRiverbendStart && today <= currentRiverbendEnd) {
-    document.getElementById('response').innerHTML = 'Nope.';
-    document.getElementById('text').innerHTML = "The riverfront is trampled & downtown is covered with trash. <br />It's almost done. Hang in there.";
-    CountDownTimer(currentRiverbendEnd, 'countdown');
-}
-else {
-    document.getElementById('response').innerHTML = 'Yep.';
-    document.getElementById('text').innerHTML = "But don't get too excited. The whole thing starts again in:";
-    CountDownTimer(nextRiverbendStart, 'countdown');
-}
-
-// Borrowed from http://stackoverflow.com/questions/9335140/how-to-countdown-to-a-date
-function CountDownTimer(end, id)
-{
-    var _second = 1000;
-    var _minute = _second * 60;
-    var _hour = _minute * 60;
-    var _day = _hour * 24;
-    var timer;
-
-    function showRemaining() {
-        var now = new Date();
-        var distance = end - now;
-        if (distance < 0) {
-
-            clearInterval(timer);
-            document.getElementById(id).innerHTML = 'EXPIRED!';
-
-            return;
-        }
-        var days = Math.floor(distance / _day);
-        var hours = Math.floor((distance % _day) / _hour);
-        var minutes = Math.floor((distance % _hour) / _minute);
-        var seconds = Math.floor((distance % _minute) / _second);
-
-        document.getElementById(id).innerHTML = days + 'days ';
-        document.getElementById(id).innerHTML += hours + 'hrs ';
-        document.getElementById(id).innerHTML += minutes + 'mins ';
-        document.getElementById(id).innerHTML += seconds + 'secs';
+    function formatDateDiff(start, end) {
+        var duration = moment.duration(end.diff(start));
+        return duration.days() + ' days ' +
+            duration.hours() + ' hrs ' +
+            duration.minutes() + ' mins ' +
+            duration.seconds() + ' secs';
     }
 
-    showRemaining();
-    timer = setInterval(showRemaining, 1000);
-}
+    function updatePage() {
+        var now = moment();
+
+        if (now.isBetween(riverbendStart, riverbendEnd)) {
+            $('.nope').show();
+            $('.countdown').html(formatDateDiff(now, riverbendEnd));
+        } else {
+            $('.yep').show();
+            $('.countdown').html(formatDateDiff(now, futureEventStart));
+        }
+    }
+
+    updatePage();
+    window.setInterval(updatePage, 1000);
+});
