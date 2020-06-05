de5a2c488ac19ab2b1bd7188f2247907b0def674
Tom Richards <tom@tomrichards.net>
2020-06-04 21:00:24 -0400

    Add missing messages

--- src/lib/messages.ts
+++ src/lib/messages.ts
@@ -5,5 +5,7 @@ export const messages = [
   // "Nope. We’re still in the throes of it. I just heard some of our beloved, local panhandlers discuss investing in cryptocurrency.",
   // "Not as long as the world contains one original, living member of Lynyrd Skynyrd.",
   // "April showers left May flowers. But Riverbenders left a bottle full of pee-pee in our parking lot.",
+  // "Pause for Riverbend's bastard child, The Strut - which ironically is the only tolerable day of of the festival.""
+  // "If you weren't able to make it to the Riverbend Run this morning, don't worry! You can still get the runs from any of the concession stands located throughout the festival grounds.",
   "As tumbleweeds of trash blow down Broad Street, the end is near."
 ];
