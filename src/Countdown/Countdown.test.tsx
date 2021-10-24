81cdc6008ee054dc0c665d4b5f141d70d3bafed6
Tom Richards <tom@tomrichards.net>
2021-10-24 16:40:46 -0400

    Fix tests

--- src/Countdown/Countdown.test.tsx
+++ src/Countdown/Countdown.test.tsx
@@ -3,10 +3,10 @@ import { render } from "@testing-library/react";
 import Countdown from "./Countdown";
 
 describe("Countdown", () => {
-  const firstDay = new Date("2019-05-29T19:00:00-04:00");
+  const firstDay = new Date("2022-06-02T19:00:00-04:00");
 
   it("renders yep before the event", () => {
-    const before = new Date("2019-05-28T19:00:00-04:00");
+    const before = new Date("2022-05-28T19:00:00-04:00");
 
     const { getByRole, getByText } = render(<Countdown now={before} />);
 
@@ -26,8 +26,8 @@ describe("Countdown", () => {
   });
 
   it("renders nope with the last day's message", () => {
-    const lastDay = new Date("2019-06-01T19:00:00-04:00");
-    
+    const lastDay = new Date("2022-06-05T19:00:00-04:00");
+
     const { getByRole, getByText } = render(<Countdown now={lastDay} />);
 
     const heading = getByRole("heading");
@@ -37,7 +37,7 @@ describe("Countdown", () => {
   });
 
   it("renders yep after the event", () => {
-    const after = new Date("2019-06-02T19:00:00-04:00");
+    const after = new Date("2022-07-02T19:00:00-04:00");
 
     const { getByRole, getByText } = render(<Countdown now={after} />);
 
@@ -55,7 +55,7 @@ describe("Countdown", () => {
   });
 
   it("renders unsure when we've past the next event date", () => {
-    const wayAfter = new Date("2020-08-29T18:15:00-04:00");
+    const wayAfter = new Date("2023-08-29T18:15:00-04:00");
 
     const { getByText } = render(<Countdown now={wayAfter} />);
 
