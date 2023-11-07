1e5fc3d6260287567ccb8081236f559b9abb92ef
Tom Richards <tom@tomrichards.net>
2023-11-06 23:42:59 -0500

    Bump date for 2025

--- src/lib/events.ts
+++ src/lib/events.ts
@@ -1,10 +1,10 @@
-// 2023
+// 2024 was cancelled!
 export const currentEvent = {
   start: new Date("2023-06-02T16:00:00-04:00"),
   end: new Date("2023-06-04T23:30:00-04:00")
 };
 
-// 2024 guess
+// 2025 guess
 export const nextEvent = {
-  start: new Date("2024-06-02T16:00:00-04:00")
+  start: new Date("2025-06-02T16:00:00-04:00")
 };
